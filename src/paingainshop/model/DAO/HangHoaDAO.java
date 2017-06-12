fe53f2eead072281f3b235e2832b108bae88dfe4
Dang Tien Chieu <dangtien2409@gmail.com>
2017-06-12 11:32:34 +0700

    tranvanhuy04

--- src/paingainshop/model/DAO/HangHoaDAO.java
+++ src/paingainshop/model/DAO/HangHoaDAO.java
@@ -21,23 +21,7 @@ public class HangHoaDAO {
     public HangHoaDAO() {
         db = new DBconnect();
     }
-    public boolean addHangHoa(HangHoa hh) throws Exception
-	{
-		String sql = "insert into HangHoa values (?,?,?,?,?,?,?,?,?,?)";
-		PreparedStatement pst = db.openConnect().prepareStatement(sql);
-		pst.setString(1, hh.getMaHH());
-		pst.setString(2, hh.getTenHH());
-		pst.setString(3, hh.getNhomHH());
-		pst.setInt(4, hh.getGiaBan());
-		pst.setInt(5, hh.getGiaNhap());
-		pst.setString(6, hh.getThuocTinh());
-		pst.setInt(7, hh.getSoLuong());
-		pst.setString(8, hh.getDonViTinh());
-		pst.setString(9, hh.getMaNCC());
-		pst.setString(10, hh.getGhiChu());
-		
-		return pst.executeUpdate()>0;
-	}
+    
     public String getLastPkey() throws Exception{
         String sql = "select MaHH from HangHoa order by MaHH DESC Limit 1";
         ResultSet rs = db.getStatement().executeQuery(sql);
